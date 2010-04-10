2a7a92c2d87941ced87329af5d00d7de87647bc6
klusark <klusark@gmail.com>
2010-04-10 14:40:18 -0700

    fixed chat

--- Server.lua
+++ Server.lua
@@ -65,6 +65,7 @@ function OnMapPostLoad()
     // Create the game object. This is a networked object that manages the game
     // state and logic.
     Server.CreateEntity("game", Vector(0, 0, 0))
+    Server.CreateEntity("chat", Vector(0, 0, 0))
 
 end
 
