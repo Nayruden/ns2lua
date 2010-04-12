65367588a466f0651b909e456362d0609943fb7c
PandaBear41 <chris@hsweb.us>
2010-04-12 10:28:42 -0500

    Fixed the problem when changing meshes in thirdperson. No more ghost meshes.

--- lua/Actor.lua
+++ lua/Actor.lua
@@ -580,6 +580,8 @@ if (Client) then
                 Client.DestroyRenderModel(self.model)
                 self.model = nil
             else
+			    Client.DestroyRenderModel(self.model)
+                self.model = nil
                 self.model = Client.CreateRenderModel()
                 self.model:SetModel(self.modelIndex)
             end
