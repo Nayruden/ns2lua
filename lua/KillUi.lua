ab74dcc16a42f62d8e10d377a48d44f507da96a9
sclark39 <sclark@freolic.com>
2010-04-13 21:02:58 -0700

    Finished implementing the light-weight kill reporting system.

--- lua/KillUi.lua
+++ lua/KillUi.lua
@@ -1,25 +1,20 @@
-KillUI_KillerLog = {}
-KillUI_KilledLog = {}
+KillUI_KillLog = { }
 
 function KillUI_AddKill(killer, killed)
-	table.insert(KillUI_KillerLog, 1, killer)
-	table.insert(KillUI_KilledLog, 1, killed)
-	Shared.Message("--KillUI 2--")	
-	Shared.Message("Killer: " .. KillUI_KillerLog[1])
-	Shared.Message("Killed: " .. KillUI_KilledLog[1])
-	Shared.Message("--KillUI 3--")	
-	Shared.Message("Killer: " .. KillUI_GetKiller(1))
-	Shared.Message("Killed: " .. KillUI_GetKilled(1))
+	table.insert(KillUI_KillLog, 1, {		
+		attacker=killer, 
+		victim=killed
+	});
 end
 
-function KillUI_GetKiller(killID)
-   return KillUI_KillerLog[ID]
+function KillUI_GetKiller(ID)
+   return KillUI_KillLog[ID].attacker
 end
 
-function KillUI_GetKilled(killID)
-   return KillUI_KilledLog[ID]
+function KillUI_GetKilled(ID)
+   return KillUI_KillLog[ID].victim
 end
 
 function KillUI_GetNumberOfKillsInLog()
-   return table.getn(KillUI_KillerLog)
+   return table.getn(KillUI_KillLog)
 end
